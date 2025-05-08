927be222bf4d0312cd472b903bcc846b7b305253
md-kudrot-592 <kk.mdkudrot@gmail.com>
2025-05-08 13:58:30 +0600

    OTP Generator function created

--- src/script.js
+++ src/script.js
@@ -9,15 +9,59 @@ function tackleOTPBoxes(){
             return;
         }
 
-        const nextElement = target.nextElementSibling
-        if(nextElement){
-            nextElement.focus();
-        }
+        const netElement = target.nextElementSibling
+        if(netElement){
+            netElement.focus();
+        }        
     })
+    
+}
+
+function generateOTP(){
+    const otp = Math.floor(1000 + Math.random() * 9000)
+    const otpElem = document.getElementById("genereted-otp-id")
+    otpElem.innerHTML = `Your OTP is : ${otp}`
 }
 
 function init(){
     tackleOTPBoxes();
+    generateOTP();
 }
 
 init();
+
+// *****************************************
+// console.log("hello")
+
+// function tackleOTPBoxes(){
+//     const otpbox = document.getElementById("otp-box-list-id")
+//     otpbox.addEventListener("input",function(e){
+//         const target = e.target
+//         const value = target.value
+//         console.log(value)
+        
+//         if(isNaN(value)){
+//             target.value = "";
+//             return;
+//         }
+//     })
+// }
+
+// tackleOTPBoxes()
+
+
+
+// function box_input(){
+//     const parent_box = document.getElementById("otp-box-id")
+//     parent_box.addEventListener("input", (e)=>{
+//         const target = e.target;
+//         const value = target.value
+//         if (isNaN(value)){
+//             target.value = "";
+//             return;
+//         }
+//     })
+
+// }
+
+// box_input();
